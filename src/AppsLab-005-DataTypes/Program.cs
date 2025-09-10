b8259851ab5d443caa5e4f69e52c9de54a45ba87
luciakr25 <luciakr25@Z00T1-03>
2025-09-10 13:09:38 +0200

    uloha 005= scitavanie odcitavanie

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -7,7 +7,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            bool isChild = true;
+            /*bool isChild = true;
             Console.WriteLine(isChild);
             
             int myAge = 15;
@@ -20,8 +20,63 @@ namespace MyApp
             Console.WriteLine(name);
 
             char gender = 'M';
-            Console.WriteLine(gender);
+            Console.WriteLine(gender);*/
 
+            int a = 10;
+            int b = 20 + 30 * 20 + 30 * 20 + 30;
+
+            int sum = a + b;
+            //Scitavanie cisla A a B
+            //Console.WriteLine(sum);
+            Console.WriteLine(a + b);
+            //Console.WriteLine(10 + 5);
+
+            //Odcitavanie cisla A a B
+            Console.WriteLine(a - b);
+
+            //Nasobe cisla A a B
+            Console.WriteLine(a * b);
+
+            //Delenie cisla A a B
+            Console.WriteLine(a / b);
+
+            Console.WriteLine("Volam sa Rado");
+
+
+
+
+
+            int birthday = 8;
+            int birthMonth = 3;
+            int birthYear = 2010;
+
+            //Scitanie datumu narodenia mesiaca a roku do premennej birthSum
+            int birthSum = birthday + birthMonth * birthYear;
+            //Vypisanie na konzolu birthSum (cez Console.Writeline)
+            Console.WriteLine(birthSum);
+            //Nasledne vynasobte birtSu * 10 a vypiste nasobok
+            Console.WriteLine(birthSum * 10);
+
+
+            //Scitanie datum narodenia a mesiac a ay potom prenasobte rokom
+            Console.WriteLine((birthday + birthMonth) * birthYear);
+
+            Console.WriteLine(5/3);
+
+            //Vytvorte si premennu s vasim meno a scitajte ju s rokom narodenia
+            string name = "Rado";
+            Console.WriteLine(name+birthYear);
+
+            //Vypiste meno a sucet dna a mesiaca narodenia
+            Console.WriteLine(name + birthday + birthMonth);
+
+
+            bool result = 6 > 3;
+            Console.WriteLine(result);
+
+            string igor = "Igor";
+            string Michal = "Igor";
+            Console.WriteLine(igor == Michal);
         }
     }
 }
