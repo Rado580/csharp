f8e61e4e46c94369243f16542b015a156d479fb9
luciakr25 <luciakr25@Z00T1-03>
2025-09-10 10:07:56 +0200

    Uloha 005=skušanie premenných

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isChild = true;
+            Console.WriteLine(isChild);
+            
+            int myAge = 15;
+            Console.WriteLine(myAge);
+
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+
+            string name = "Rado";
+            Console.WriteLine(name);
+
+            char gender = 'M';
+            Console.WriteLine(gender);
+
+        }
+    }
+}
