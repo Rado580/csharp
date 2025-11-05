544c05e3092820234f86fff5cd2e2aad5695aac9
luciakr25 <luciakr25@Z00T1-03>
2025-11-05 12:41:52 +0100

    ulohz s randomgenerator cisiel

---
+++ Cvicenie_RandomGeneratorCisel/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Cvicenie_RandomGeneratorCisel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int ct = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                bool result = Prob(r);
+                if (result == true);
+                {
+                    ct++;
+                }
+            }
+            Console.WriteLine("True bolo: " +ct+ "x");
+        }
+
+        public static bool Prob(Random r )
+        {
+            
+            int nahodneCislo = r.Next(0, 101);
+            if (nahodneCislo >= 73) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ nahodne mena/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nahodne_mena
+{
+    public class Person
+    {
+        public string Name { get; set; } = "Peter","Martin","Jozef","Tomáš","Lukáš","Jakub","Michal","Marek","Andrej","Anton",
+              "Samuel","Dominik","Richard","Róbert","Roman","Patrik","Filip","Juraj","Karol","Daniel",
+              "Dávid","Adam","Erik","Igor","Ivan","Ľubomír","Pavol","Stanislav","Štefan","Matej",
+              "Matúš","Vladimír","Viliam","Radovan","Rastislav","Šimon","Sebastián","Eduard","Marián","Gabriel",
+              "Tobiáš","Boris","Alan","Oliver","Oskar","Leonard","Viktor","Alex","Benjamin","Tadeáš"
+
+        public string Surname { get; set; } = "Novák""Horváth","Kováč","Varga","Tóth","Nagy","Molnár","Szabó","Lukáč","Kučera",
+              "Polák","Kráľ","Urban","Krajčí","Kováčik","Kubiš","Holub","Benko","Blaško","Marek",
+              "Tomáš","Baláž","Farkaš","Bartoš","Šimko","Pavlík","Michalík","Černák","Švec","Ševčík",
+              "Jurík","Hruška","Chovanec","Rác","Hudák","Ondruš","Sokol","Pospíšil","Gašpar","Gregor",
+              "Valent","Ďurica","Ďuriš","Vavro","Žák","Koreň","Červeň","Šramko","Mráz","Mrva"
+
+
+
+    }
+}
